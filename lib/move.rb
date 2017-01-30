8ae701119c7380236618795391021c621c88216e
Mohammed  Youssef <mohamed.saleh@vet.svu.edu.eg>
2017-01-30 18:05:33 +0000

    Done.

--- lib/move.rb
+++ lib/move.rb
@@ -1,9 +1,20 @@
+
 def display_board(board)
   puts " #{board[0]} | #{board[1]} | #{board[2]} "
   puts "-----------"
   puts " #{board[3]} | #{board[4]} | #{board[5]} "
   puts "-----------"
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
+
+def input_to_index (move)
+   move = move.to_i - 1
+   return move
+end
+
+def move(b_array, index, player_character = 'X')
+  b_array[index] = player_character
+  return b_array
 end
 
 # code your input_to_index and move method here!

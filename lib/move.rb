ffac35282422af02371bb583230646b33b41f19d
ahmad sulasi <ahmad.luthfi@student.upi.edu>
2017-03-01 11:11:09 +0000

    Done.

--- lib/move.rb
+++ lib/move.rb
@@ -7,3 +7,10 @@ def display_board(board)
 end
 
 # code your input_to_index and move method here!
+def input_to_index(masuk)
+  index=masuk.to_i
+  index=index-1
+end
+def move(board,poskarakter,plkar="X")
+  board[poskarakter]=plkar
+end

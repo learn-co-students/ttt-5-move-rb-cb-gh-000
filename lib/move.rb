57b37e3790bb77b7f4a7552a51b361f04eb6ec1e
Andre Mgowano <amgowano.apm@gmail.com>
2017-03-06 12:22:33 +0000

    Done.

--- lib/move.rb
+++ lib/move.rb
@@ -7,3 +7,11 @@ def display_board(board)
 end
 
 # code your input_to_index and move method here!
+def input_to_index(user_input)
+  player_move_int= user_input.to_i
+  return board_index = player_move_int - 1
+end
+
+def move (board,board_index,player_symbol="X")
+  board[board_index]=player_symbol
+end

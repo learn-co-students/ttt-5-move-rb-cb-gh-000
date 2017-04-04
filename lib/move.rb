24da92cf5eec06a7ec1bfc3cca30feac7dc462db
Peter Czupil <pjczupil@gmail.com>
2017-04-04 07:02:05 +0000

    Done.

--- lib/move.rb
+++ lib/move.rb
@@ -7,3 +7,13 @@ def display_board(board)
 end
 
 # code your input_to_index and move method here!
+def input_to_index(num)
+  if num.to_i >= 1 and num.to_i <= 9
+    return num.to_i - 1
+  end
+  return -1
+end
+
+def move(board, index, character = "X")
+  board[index] = character
+end

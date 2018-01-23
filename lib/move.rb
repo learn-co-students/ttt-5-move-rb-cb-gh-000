cac091119dd2ad976e94f0a537e0ae15cf047b7b
Daniel Echeverry Giraldo <dfecheverryg@unal.edu.co>
2018-01-23 00:35:50 +0000

    Done.

--- lib/move.rb
+++ lib/move.rb
@@ -7,3 +7,23 @@ def display_board(board)
 end
 
 # code your input_to_index and move method here!
+
+def input_to_index(cadena)
+  index = cadena.to_i
+  index -= 1
+end
+
+def move(board, index, caracter = "X")
+  board[index] = caracter
+end
+
+#move(board, 0, "X")
+#move(board, 1, "O")
+#move(board, 2, "X")
+#move(board, 3, "O")
+#move(board, 4, "X")
+#move(board, 5, "O")
+#move(board, 6, "X")
+#move(board, 7, "X")
+#move(board, 8, "O")
+#display_board(board)

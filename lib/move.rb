6ba92fa838e1f78f96d99aebd14880582d1eca40
Leroy Hutchinson <l.hutchinsonj@gmail.com>
2018-06-16 00:10:01 +0000

    Done.

--- lib/move.rb
+++ lib/move.rb
@@ -7,3 +7,15 @@ def display_board(board)
 end
 
 # code your input_to_index and move method here!
+
+
+#Takes user input and uses it to fill out position that user inputed
+def input_to_index(input = 0)
+
+index = input.to_i - 1
+return index
+end
+
+def move(array, index, value = "X")
+  array[index] = value
+end

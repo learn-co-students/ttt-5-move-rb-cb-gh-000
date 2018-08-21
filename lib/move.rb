08306d7a97733c9e4563807498601b21d848f64f
darren mcgregor <darren.mcgregor@avanzac.co.uk>
2018-08-21 16:06:14 +0000

    Done.

--- lib/move.rb
+++ lib/move.rb
@@ -6,4 +6,17 @@ def display_board(board)
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
+def input_to_index(user_input)
+  puts "Please enter a number between 1 and 9"
+  user_input = user_input.to_i
+  user_input -= 1
+  return user_input
+end
+
+def move(board, index, character = "X")
+  board[index] = character
+  return board
+end
+
+
 # code your input_to_index and move method here!

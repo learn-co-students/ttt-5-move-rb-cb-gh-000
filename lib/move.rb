23823e58bea8ecd8013805216da04ebfababae84
Sam Json <3115002443@mail2.gdut.edu.cn>
2018-12-02 03:08:08 +0000

    Done.

--- lib/move.rb
+++ lib/move.rb
@@ -7,3 +7,15 @@ def display_board(board)
 end
 
 # code your input_to_index and move method here!
+def input_to_index input
+  input = input.to_i
+  if input <= 9 and input >= 1
+    index = input - 1
+  else
+    -1
+  end
+end
+
+def move board,index,value='X'
+  board[index] = value
+end

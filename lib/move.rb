e6a52d5c3eccea124949b4e6bce8f943fbf731d6
Luí s Fernando Guedes <lfernandoguedes@gmail.com>
2019-01-29 15:13:42 +0000

    Done.

--- lib/move.rb
+++ lib/move.rb
@@ -1,4 +1,4 @@
-def display_board(board = [" ", " ", " ", " ", " ", " ", " ", " ", " "])
+def display_board(board)
   puts " #{board[0]} | #{board[1]} | #{board[2]} "
   puts "-----------"
   puts " #{board[3]} | #{board[4]} | #{board[5]} "
@@ -6,15 +6,12 @@ def display_board(board = [" ", " ", " ", " ", " ", " ", " ", " ", " "])
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
-def input_to_index(input)
-  return input.to_i - 1
+# code your input_to_index and move method here!
+
+def input_to_index(user_input)
+  user_input.to_i - 1
 end
 
-def move(board, position = -1, char = "X")
-  if board.kind_of?(Array)
-    board[position] = char
-    display_board(board)
-  else
-    display_board()
-  end
+def move(board, index, current_player = "X")
+  board[index] = current_player
 end

e9c95cb19f740858fd87873a3d07cb596ae9d9bc
Farzan Khoobchehr <farzan_khoobchehr@yahoo.com>
2019-05-25 22:37:54 +0000

    Done.

--- lib/move.rb
+++ lib/move.rb
@@ -6,4 +6,10 @@ def display_board(board)
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
-# code your input_to_index and move method here!
+def input_to_index(farfar)
+  farfar.to_i - 1
+end
+
+def move(board, index, user_marker = "X")
+  board[index.to_i] = user_marker
+end

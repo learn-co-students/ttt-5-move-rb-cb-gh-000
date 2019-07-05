eee4ffb7049232001c8972f189913a9e87c002cf
Atul Tiwari <atultiwarix7@gmail.com>
2019-07-05 12:37:27 +0000

    Done.

--- lib/move.rb
+++ lib/move.rb
@@ -11,7 +11,8 @@ def input_to_index(input)
   input=input.to_i
   return index=input-1
 end
-def move(board,index,value="X)
+
+def move(board,index,value="X")
   board[index]=value
   return board
 end

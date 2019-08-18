d0c0b4a843439e8dd64f94706a357300034a81c4
Ben Francisco <bpfranci@math.tamu.edu>
2019-08-18 21:04:59 +0000

    Done.

--- lib/move.rb
+++ lib/move.rb
@@ -7,3 +7,21 @@ def display_board(board)
 end
 
 # code your input_to_index and move method here!
+#input_to_index convert a move string to the appropriate array index.
+def input_to_index(move)
+  move.to_i - 1
+end
+
+# move: method to record a move.
+#
+# =>  array - the board array before the move is made.      (array of strings)
+# =>  index - the board index where the move is to be made. (integer)
+# =>  value - the symbol associated with the move.          (string)
+#
+# <=  array - the board after the move is made.             (array of strings)
+def move(array, index, value = "X")
+  # record the move
+  array[index] = value
+  # return the array
+  return array
+end

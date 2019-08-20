2bc958f77bb4e9343393148d273551609dc1a357
Duc Luu Van <duc.lv172482@sis.hust.edu.vn>
2019-08-20 14:29:18 +0000

    Done.

--- lib/move.rb
+++ lib/move.rb
@@ -6,4 +6,15 @@ def display_board(board)
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
+def input_to_index(k)
+  return k.to_i-1
+end
+
+def move(board,x,k="X")
+  board[x.to_i]=k
+end
+
+def update_array_at_with(array, index, value)
+  array[index] = value
+end
 # code your input_to_index and move method here!
